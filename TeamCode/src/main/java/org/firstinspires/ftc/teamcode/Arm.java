9d2b7a8825bc105c91e8e4f1e760ff25a1c9003f
Matthew <mgiordantx@gmail.com>
2017-12-08 21:06:46 -0500

    Arm.java now recognizes the arm motor and analog sensor.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 /**
  * Created by Glenn on 12/08/2017.
  */
@@ -7,7 +10,14 @@ package org.firstinspires.ftc.teamcode;
 public class Arm
 {
 
+    private DcMotor armMotor = null;
+    private AnalogInput armSensor = null;
 
 
+    public Arm(DcMotor aMotor, AnalogInput aSensor)
+    {
+        armMotor = aMotor;
+        armSensor = aSensor;
+    }
 
 }
